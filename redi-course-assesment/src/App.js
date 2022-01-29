becaa0e2a704ec5f68c549c93dac45b969d210f7
Rehnuma <rehnuma.lamia@gmail.com>
2022-01-29 16:21:12 +0100

    initial project setup

---
+++ redi-course-assesment/src/App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./pages/dasboard/dasboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ redi-course-assesment/src/pages/allCourses/allCourses.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import style from "../dasboard/index.css";
+
+function AllCourses() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+export default AllCourses;
