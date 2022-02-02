3cfa9ff9e34d35d9d83179e368f6413e0ac97337
Rehnuma <rehnuma.lamia@gmail.com>
2022-02-02 23:07:27 +0100

    folder restructure

---
+++ redi-course-assesment/src/pages/DashboardPage.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import { Button } from "../components/Button";
+
+export function DashboardPage() {
+  return (
+    <main>
+      <h1>
+        Are you interested in taking Web Development Course in Digital Career
+        Program?
+      </h1>
+      <Button />
+    </main>
+  );
+}
+
+//export default DashboardPage;
---
+++ redi-course-assesment/src/pages/DecisionPage.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+function DecisionPage() {
+  return (
+    <div>
+      <h1>This is DecisionPage</h1>
+    </div>
+  );
+}
+export default DecisionPage;
