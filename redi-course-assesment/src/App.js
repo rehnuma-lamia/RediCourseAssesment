7fad0b354293c6edb7d945767ab3a7c5ebde7204
Rehnuma <rehnuma.lamia@gmail.com>
2022-02-04 16:00:20 +0100

    routing added

--- redi-course-assesment/src/App.js
+++ redi-course-assesment/src/App.js
@@ -1,11 +1,20 @@
 import { Header } from "./components/Header";
-import { DashboardPage } from "./pages/DashboardPage";
+import { Routes, Route } from "react-router-dom";
+
+// PAGES
+import DashboardPage from "./pages/DashboardPage";
+import DecisionPage from "./pages/DecisionPage";
+import QuestionListPage from "./pages/QuestionListPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <DashboardPage />
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/decision" element={<DecisionPage />} />
+        <Route path="/questions" element={<QuestionListPage />} />
+      </Routes>
     </div>
   );
 }
