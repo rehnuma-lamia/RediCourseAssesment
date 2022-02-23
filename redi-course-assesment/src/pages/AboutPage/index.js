727f45ee728613535b13464bf8cf0eae2966d6eb
Rehnuma <rehnuma.lamia@gmail.com>
2022-02-23 17:08:58 +0100

    - about page added

--- redi-course-assesment/src/pages/AboutPage/index.js
+++ redi-course-assesment/src/pages/AboutPage/index.js
@@ -5,7 +5,23 @@ import redi from "../../images/redi.jpeg";
 import "./index.css";
 
 function AboutPage() {
-  let items = ["ishtiak", "lamia", "Zavian", "nnm", "ooo"]
+  let items = [
+    {
+      title: "You can do self Assesment test for Web development courses",
+      details:
+        "To do a assesment You know the basic use of the computer (eg. email,web browser, Excel, PowerPoint). You can communicate in English speaking, listening and writing). You are 18 years and older We givepriority to people with a forced migration background.",
+    },
+    {
+      title: "What is course assessment?",
+      details:
+        "Course-level assessment is a process of systematically examining and refining the fit between the course activities and what students should know at the end of the course.",
+    },
+    {
+      title: "Why do you need course assessment?",
+      details:
+        "Couse assessment is needed to figure out the right course for you.",
+    },
+  ];
   return (
     <div>
       <img className="aboutTopImage" alt="redi" src={redi} />
@@ -13,20 +29,14 @@ function AboutPage() {
 
       {
         <div>
-          {items.map((value) => (
-            <div>{value}</div>
+          {items.map((item) => (
+            <div className="aboutPageDesign">
+              <h3>{item.title}</h3>
+              <p>{item.details}</p>
+            </div>
           ))}
         </div>
       }
-      <div className="aboutPageDesign">
-        <h3>You can do self Assesment test for Web development courses</h3>
-        <p>
-          To do a assesment You know the basic use of the computer (eg. email,
-          web browser, Excel, PowerPoint). You can communicate in English
-          (speaking, listening and writing). You are 18 years and older We give
-          priority to people with a forced migration background.
-        </p>
-      </div>
     </div>
   );
 }
