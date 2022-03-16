5c2616836796b8bcf59c2a6163c91ec582fb93fb
Rehnuma <rehnuma.lamia@gmail.com>
2022-03-16 13:50:22 +0100

    Score and course title fixed

--- redi-course-assesment/src/pages/AboutPage/index.js
+++ redi-course-assesment/src/pages/AboutPage/index.js
@@ -7,11 +7,6 @@ import "./index.css";
 function AboutPage() {
   let items = [
     {
-      title: "You can do self Assesment test for Web development courses",
-      details:
-        "To do a assesment You know the basic use of the computer (eg. email,web browser, Excel, PowerPoint). You can communicate in English speaking, listening and writing). You are 18 years and older We givepriority to people with a forced migration background.",
-    },
-    {
       title: "What is course assessment?",
       details:
         "Course-level assessment is a process of systematically examining and refining the fit between the course activities and what students should know at the end of the course.",
@@ -20,6 +15,11 @@ function AboutPage() {
       title: "Why do you need course assessment?",
       details:
         "Couse assessment is needed to figure out the right course for you.",
+    },
+    {
+      title: "To become a student",
+      details:
+        "You know the basic use of the computer (eg. email, web browser, Excel, PowerPoint).You can communicate in English (speaking, listening and writing).You are 18 years and older We give priority to people with a forced migration background.",
     },
   ];
   return (
